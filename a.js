bca20946f456382b020b481b68520d422a9dae15
Pushpendra6263 <gulludubey123@gmail.com>
2022-09-30 20:25:13 +0530

    first commit

---
+++ a.js
@@ -0,0 +1,92 @@
+gsap.to("#b1h3-div h3",{
+    duration : 2,
+    top : 0,
+    stagger : 2,
+    ease : Expo.easeInOut
+})
+gsap.to("#b1h3-div h3",{
+    delay : 2,
+    top : "-100%",
+    stagger : 2,
+    ease : Expo.easeInOut
+})
+
+
+gsap.to("#b2 .b2-class",{
+        duration : 2,
+        left : 0,
+        stagger : 2,
+        ease : Expo.easeInOut
+})
+
+
+gsap.to("#b3 .b3-class",{
+    duration : 2,
+    top : 0,
+    stagger : 2,
+     ease : Expo.easeInOut
+})
+
+
+gsap.to("#b3 .b3p2-class",{
+    duration : 2,
+    left : 0,
+    stagger : 2,
+    ease : Expo.easeInOut
+})
+
+
+gsap.to("#last-div .last",{
+    duration : 2,
+    top : 0,
+    stagger : 2,
+    ease : Expo.easeInOut
+})
+gsap.to("#last-div .last",{
+    delay : 2,
+    top : "-100%",
+    stagger : 2,
+    ease : Expo.easeInOut
+})
+
+
+gsap.to("#dots .dot",{
+    duration : 1.5,
+    stagger : 1.5,
+    ease : Expo.easeInOut,
+    backgroundColor : "Red"
+})
+gsap.to("#dots .dot",{
+    delay : 2.5,
+    stagger : 2.5,
+    ease : Expo.easeInOut,
+    backgroundColor : " rgba(255, 255, 255, 0.472)"
+})
+
+
+gsap.to("#nav .circle",{
+    duration : 1,
+    stagger : 2,
+    ease : Expo.easeInOut,
+    backgroundColor : "Red"
+})
+
+gsap.to("#bottom1 h1",{
+    duration : 2,
+    top : 0,
+    stagger : 2,
+    ease : Expo.easeInOut
+})
+gsap.to("#bottom1 h1",{
+    delay : 2,
+    top : "-100%",
+    stagger : 2,
+    ease : Expo.easeInOut
+})
+
+gsap.to(".top-pic",{
+    duration : 2,
+    left : 0,
+    stagger : 2,
+    ease : Expo.easeInOut
+})
